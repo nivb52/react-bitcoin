774db503f962a5c5efcac133c76ba7cbcf961889
nivb52 <nivb52@gmail.com>
2019-08-20 16:09:46 +0300

    adding chart with sparklines

--- src/cmps/Chart.js
+++ src/cmps/Chart.js
@@ -3,3 +3,27 @@ Render a chart
 Props for example -: title, data, description, color
 LOOK IN PDF FOR THE APIS
 */
+
+import React, { Component } from 'react'
+import { Sparklines, SparklinesLine, SparklinesSpots } from 'react-sparklines';
+
+export default class Chart extends Component {
+    render() {
+        let { name, description, period, points } = this.props.data
+        let color = this.props.color || 'black'
+        return (
+            <div className="chart">
+                <p> {name} - for a {period} </p>
+                <p> {description} </p>
+
+                <Sparklines data={points}
+                >
+                    <SparklinesLine color={color} 
+                    style={{ strokeWidth: 1 , fillOpacity: .1}}
+                    />
+                    <SparklinesSpots  style={{ fill: "black" }} />
+                </Sparklines>
+            </div>
+        )
+    }
+}
