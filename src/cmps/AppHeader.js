cedb4337539ba86d50f5a2d3e013f71539c97857
nivb52 <nivb52@gmail.com>
2019-08-20 23:30:17 +0300

    change header text

---
+++ src/cmps/AppHeader.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import UtillService from '../services/UtillService'
+import {Logo} from './icons/Logo'
+
+export default class Header extends Component {
+    render() {
+const month = new Date().getMonth()
+const monthName = UtillService.getMonthByName(month)
+        return (
+
+            <header 
+            className="logo-container flex justify-center 
+            align-center pad-1rem">
+
+                <h1 className="subtitle is-3 text-white marginless">Coding Academy</h1>
+                <h2 className="text-white">{monthName} 2019</h2>
+                
+            </header>
+        )
+    }
+}
