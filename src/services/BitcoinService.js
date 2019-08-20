8e3da9f7f1d6e32907c324162c260a23168ed22d
nivb52 <nivb52@gmail.com>
2019-08-20 16:13:52 +0300

    create bitcoin service for charts

--- src/services/BitcoinService.js
+++ src/services/BitcoinService.js
@@ -1,23 +1,73 @@
+import { storageService } from './StorageService.js'
+import axios from 'axios'
+// const axios = require('axios');
+
+const RATE_KEY = 'btc_rate'
+const MARKET_KEY = 'market_price'
+const LAST_VISIT_KEY = 'date'
+
+
 export default {
-    getRate,
+    // getRate,
+    getRatePrm,
     getMarketPrice,
     getConfirmedTransactions
-    //chart
-  }
+}
 
 
-  async function getRate(coins) {
-      try{
+function getRatePrm(coins) {
+    var rate = storageService.load(RATE_KEY)
 
-      }catch(err) {
+    if (!rate || _is24hrFromLastFetch()) {
+        return axios.get(`https://blockchain.info/tobtc?currency=USD&value=${coins}&cors=true`)
+            .then(res => {
+                rate = res.data
+                storageService.store(RATE_KEY, rate)
+                storageService.store(LAST_VISIT_KEY, Date.now())
 
-      }
+                return rate
+            })
+    }
+    return rate
+}
 
-  }
+// ::::::::::::::::::::::::::::::
+// ::::::: CHART DATA :::::::::::
+// ::::::::::::::::::::::::::::::
+async function getMarketPrice() {
+    // https://api.blockchain.info/charts/market-cap?format=json
+    var marketPrice = storageService.load(MARKET_KEY)
 
-  function getMarketPrice() {}
+    if (!marketPrice || _is24hrFromLastFetch()) {
+        const res = await axios.get(`https://api.blockchain.info/charts/market-price?format=json&cors=true`)
+        marketPrice = res.data
+        // DOT VALUS IS THE DATA OF CHART
+        storageService.store(MARKET_KEY, marketPrice)
+        storageService.store(LAST_VISIT_KEY, Date.now())
+    }
+    
+    return {
+        status: marketPrice.status,
+        name: marketPrice.name,
+        description: marketPrice.description,
+        period: marketPrice.period,
+        points: marketPrice.values.map(coord => coord.y)
+    } 
+}
 
-  
-  function getConfirmedTransactions() {
+function getConfirmedTransactions() {
 
-  }
+}
+
+
+
+
+// FETCH ONCE A DAY 
+function _is24hrFromLastFetch() {
+    const lastVisit = storageService.load(LAST_VISIT_KEY) || 0
+    const currDate = Date.now()
+
+    // 86400000 = 24 HOURS
+    if (currDate >= lastVisit + 86400000) return true
+    else return false
+}
