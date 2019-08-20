4f7e1ccf081c048a316f3d7885535a31f22438b8
nivb52 <nivb52@gmail.com>
2019-08-20 16:13:08 +0300

    create blank homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,2 +1,11 @@
 // Use UserService.getUser and BitcoinService and display:
 //  User Name and Coins
+import React from 'react';
+
+export default function Home() {
+    return (
+      <div>
+        <h1>Home</h1>
+      </div>
+    );
+  }
