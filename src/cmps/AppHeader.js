267ba6d6154688d7c6d926a8cac32da58428fa51
nivb52 <nivb52@gmail.com>
2019-08-22 17:14:51 +0300

    remove unused imports

--- src/cmps/AppHeader.js
+++ src/cmps/AppHeader.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import UtillService from '../services/UtillService'
-// import LogoContainer from './LogoContainer' 
+
+import '../css/header.css'
 
 export default class Header extends Component {
     render() {
@@ -10,14 +11,15 @@ export default class Header extends Component {
         return (
 
             <header 
-            className="flex justify-center 
+            className="header-container flex justify-center 
             align-center pad-1rem">
 
                 <h1 className="subtitle is-3 text-white marginless">
                     Coding Academy</h1>
                 <h2 className="text-white">{monthName} 2019</h2>
-                {/* <LogoContainer/> add css class logo-container to header*/}
             </header>
         )
     }
 }
+
+// this.props.history.location.pathname
