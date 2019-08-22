d59e5f34f518d42ce4279c89ae48973030263041
nivb52 <nivb52@gmail.com>
2019-08-22 16:52:39 +0300

    add func handleSort

--- src/cmps/ContactFilter.js
+++ src/cmps/ContactFilter.js
@@ -7,31 +7,47 @@ import React from 'react'
 // import './contact-filter.css';
 
 export default class ContactFilter extends React.Component {
-    
-    render() {
-        
-        return (
-
-            <section className="flex flex-row justify-center">
-                <div  className="text-grey">
-                    {
-                        <input className="input-box"
-                            onChange={this.handleChange}
-                        />
-                    }
-                    <div className="inline mar-left-1rem ">Sort By:</div>
-                    <div className="inline mar-left-1rem  mar-top-1rem pointer">Name</div> /
-                <div className="inline mar-left-1rem  mar-top-1rem pointer">Email</div>
-                </div>
-            </section>
-        )
-    }
     handleChange(el) {
         console.log(el.target.value);
         // this.props.filterBy.term = el.target.value
         // this.setState({ filterBy: {term: el.target.value} });
     }
- 
+
+    handleSort(el) {
+        console.log('handleSort', el.currentTarget);
+        // this.props.filterBy.term = el.target.value
+        // this.setState({ filterBy: {term: el.target.value} });
+    }
+    render() {
+
+        return (
+
+            <section className="flex flex-row justify-center">
+                <div className="text-grey">
+                    {
+                        <input className="input-box"
+                            onChange={this.handleChange}
+                        />
+                    }
+                    <div className="inline  mar-left-1rem  ">
+                        Sort By:
+                    </div>
+
+                    <button onClick={this.handleSort} data-sort="name"
+                        className="inline  transparent text-grey mar-left-1rem  mar-top-1rem pointer">
+                            Name
+                    </button>
+                    <span>&nbsp;/</span>
+                <button onClick={this.handleSort} data-sort="email"
+                        className="inline  transparent text-grey mar-left-1rem  mar-top-1rem pointer">
+                        Email
+                        </button>
+                </div>
+            </section>
+        )
+    }
+    
+
 
 
 }
