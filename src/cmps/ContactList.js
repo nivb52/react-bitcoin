f70b2e6c1cacfe8984b392fedae21d1ba4e59fde
nivb52 <nivb52@gmail.com>
2019-08-22 19:17:57 +0300

    reorder

--- src/cmps/ContactList.js
+++ src/cmps/ContactList.js
@@ -7,16 +7,17 @@ import { withRouter } from "react-router-dom";
 class ContactList extends React.Component {
 
     handleNew = () => {
-
         this.props.history.push('/contact/edit/')
-        // document.querySelector('a').addEventListener('')
     }
 
     render() {
         return (
             <section className="contact-center" >
                 <div className="add-user-pos">
-                    <button className="add-user prim-bcg" onClick={this.handleNew}>&#43;</button>
+                    <button 
+                        className="add-user prim-bcg" 
+                        onClick={this.handleNew}>&#43;
+                    </button>
                 </div>
                 {this.props.contacts.map(currContact =>
                     <ContactPreview contact={currContact}
