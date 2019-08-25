d22c24a1e318498dbf796b7a88a6753ad478d21b
nivb52 <nivb52@gmail.com>
2019-08-25 03:38:18 +0300

    added

--- src/cmps/TransferFund.js
+++ src/cmps/TransferFund.js
@@ -6,3 +6,40 @@
 UserService.
 */
 
+// LETS BUILD REACT HOOK
+import React from 'react'
+import '../css/transfer-fund.css'
+
+const transferFund = props => {
+    return (
+    <React.Fragment>
+        <section className="container">
+            <div className="is-medium">
+                <span > Transfer coins to</span>
+                <span className="capitalized"> Name</span>
+            </div>
+            <div className="flex-row">
+
+                <input className="input-box transfer-input"
+                    placeholder="฿"
+                    type="number" min="0" max="25" required
+                />
+                <span> &nbsp;&nbsp;&nbsp;</span>
+                <button
+                    className="transfer-btn btn-outline prim-bcg capitalized width-medium pointer">
+                    ↪
+                    {/* → */}
+                    {/* ⇒ */}
+                    {/* ⇾ */}
+                    {/* ฿ */}
+                </button>
+            </div>
+        </section>
+
+
+    </React.Fragment>
+    );
+}
+
+
+export default transferFund
