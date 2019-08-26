36916cddecff25e591b55b18e372e9b352e7d0b6
nivb52 <nivb52@gmail.com>
2019-08-26 23:14:07 +0300

    created

--- src/cmps/MovesList.js
+++ src/cmps/MovesList.js
@@ -1,3 +1,36 @@
 // - MovesList  props: title, moves
-// 
 
+import React from 'react'
+// import '../css/moves-list.css'
+
+class movesList extends React.Component {
+
+
+    render() {
+        const { lastMoves } = this.props
+        const title = lastMoves ? 'last 3 transactions' : ' no last moves'
+        
+        return (
+            <section>
+                
+                <h3 className="size-5 text-center capitalized"> {title} </h3>
+                <ol>
+                {
+                        lastMoves &&
+                        lastMoves.map((move, index) =>
+                        <li key={index}>{move}</li>
+                        )   
+                    }
+                </ol>
+
+
+
+            </section>
+
+        );
+
+    }
+
+}
+
+export default movesList
